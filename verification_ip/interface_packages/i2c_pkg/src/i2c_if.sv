aad4bc93073efeac6e37a1048a796e0fb8f39c56
vgshitol <vgshitol@ncsu.edu>
2019-02-19 02:39:37 -0500

    Resolving Errors

--- verification_ip/interface_packages/i2c_pkg/src/i2c_if.sv
+++ verification_ip/interface_packages/i2c_pkg/src/i2c_if.sv
@@ -11,36 +11,40 @@ interface i2c_if       #(
 //i2c_op_t op;
 
 // *****************************************************************************
-	task automatic wait_for_i2c_transfer (
+	task wait_for_i2c_transfer (
 		//output i2c_op_t op,
-		ref bit [I2C_DATA_WIDTH-1:0] write_data []
+		bit [I2C_DATA_WIDTH-1:0] write_data []
 	);
-		bit [I2C_ADDR_WIDTH-1:0] addr_slave;
-		bit [I2C_DATA_WIDTH-1:0] data_slave;
+		logic [I2C_ADDR_WIDTH-1:0] addr_slave;
+		logic  [I2C_DATA_WIDTH-1:0] data_slave;
+		bit sda_o;
 
-		wait(!(!sda && scl)); // Start Condition Check
+		assign sda = sda_o ? 1b'z : 0;
+
+		while(!(!sda && scl)); // Start Condition Check
 
         // Get Address and Operation
-        for(int i = 0; i < I2C_ADDR_WIDTH; i++)begin
-			@posedge(scl); begin
+        for(int i = 0; i < I2C_ADDR_WIDTH; i++) begin
+			@(posedge scl);
 				addr_slave[i] <= sda;
-			end
+			
         end
 
 //		if((addr_slave[I2C_ADDR_WIDTH-1] == 0)) op = READ;
 //		else op = WRITE;
-
-		@posedge(scl); sda <= 0; // Send Acknowledge
+		
+	
+		@(posedge scl) sda_o <= 0 ; // Send Acknowledge
 
 		// Get Data and Operation
 	//	do begin
 			for(int i = 0; i < I2C_DATA_WIDTH; i++) begin
-				@posedge(scl); data_slave[i] <= sda;
+				@(posedge scl); data_slave[i] <= sda;
 			end
 
-			@posedge(scl); sda <= 0; // Send Acknowledge
+			@(posedge scl); sda_o <= 0; // Send Acknowledge
 
-			write_data = new [write_data.size() + 1][8](write_data);
+			write_data = new [write_data.size+1](write_data);
 			write_data[write_data.size() - 1] <= data_slave;
 
 	//	end while();
@@ -58,7 +62,7 @@ interface i2c_if       #(
 // *****************************************************************************
 	task monitor (	
 		output bit [I2C_ADDR_WIDTH-1:0] addr, 
-		output i2c_op_t op,	
+		//output i2c_op_t op,	
 		output bit [I2C_DATA_WIDTH-1:0]	data[]
 	);
 	endtask
