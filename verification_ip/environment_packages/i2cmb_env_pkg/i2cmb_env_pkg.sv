d8e2d418f6a546f39147669b3442fca320b8299f
vgshitol <vgshitol@ncsu.edu>
2019-03-14 21:06:24 -0400

    Created New Files

---
+++ verification_ip/environment_packages/i2cmb_env_pkg/i2cmb_env_pkg.sv
@@ -0,0 +1,11 @@
+package i2cmb_pkg;
+
+  `include "src/i2cmb_test.svh"
+  `include "src/i2cmb_generator.svh"
+  `include "src/i2cmb_env_configuration.svh"
+  `include "src/i2cmb_environment.svh"
+  `include "src/i2cmb_predictor.svh"
+  `include "src/i2cmb_scoreboard.svh"
+  `include "src/i2cmb_coverage.svh"
+
+endpackage
---
+++ verification_ip/interface_packages/i2c_pkg/i2c_pkg.sv
@@ -0,0 +1,9 @@
+package i2c_pkg;
+
+  `include "src/i2c_configuration.svh"
+  `include "src/i2c_agent.svh"
+  `include "src/i2c_driver.svh"
+  `include "src/i2c_monitor.svh"
+  `include "src/i2c_transaction.svh"
+ 
+endpackage
