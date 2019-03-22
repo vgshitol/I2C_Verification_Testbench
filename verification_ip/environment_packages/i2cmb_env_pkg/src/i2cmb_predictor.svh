8179d98fca890529f625b9571d7ee9231c9b2b44
Vishal Shitole <vgshitol@ncsu.edu>
2019-03-22 15:43:14 -0400

    [BUGS][FIX] predictor, monitor, agent errors resolved

--- verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
+++ verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
@@ -1,6 +1,6 @@
 class i2cmb_predictor extends ncsu_component#(.T(wb_transaction));
 
-    ncsu_component#(i2c_transaction) scoreboard;
+    ncsu_component_base scoreboard;
     i2c_transaction transport_trans;
     i2cmb_env_configuration configuration;
 
@@ -12,7 +12,7 @@ class i2cmb_predictor extends ncsu_component#(.T(wb_transaction));
         configuration = cfg;
     endfunction
 
-    virtual function void set_scoreboard(ncsu_component #(i2c_transaction) scoreboard);
+    virtual function void set_scoreboard(ncsu_component_base scoreboard);
         this.scoreboard = scoreboard;
     endfunction
 
--- verification_ip/interface_packages/i2c_pkg/src/i2c_monitor.svh
+++ verification_ip/interface_packages/i2c_pkg/src/i2c_monitor.svh
@@ -23,9 +23,9 @@ class i2c_monitor extends ncsu_component#(.T(i2c_transaction));
             monitored_trans = new("monitored_trans");
             bus.monitor(
                 monitored_trans.monitor_address,
-                monitored_trans.monitor_data,
-                monitored_trans.monitor_op
-                );
+                monitored_trans.monitor_op,
+                monitored_trans.monitor_data
+            );
             $display("%s i2c_monitor::run() Address: 0x%x Data: 0x%p Operation: 0x%x",
                 get_full_name(),
                 monitored_trans.monitor_address,
