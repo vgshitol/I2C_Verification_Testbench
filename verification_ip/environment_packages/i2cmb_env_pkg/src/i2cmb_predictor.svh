98c01c0d0217d10d667b35950e3c7614bbc04128
Vishal Shitole <vgshitol@ncsu.edu>
2019-03-22 15:49:52 -0400

    [BUGS][FIX] scoreboard

--- verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
+++ verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
@@ -1,6 +1,6 @@
 class i2cmb_predictor extends ncsu_component#(.T(wb_transaction));
 
-    i2cmb_scoreboard scoreboard;
+    ncsu_component#(i2c_transaction) scoreboard;
     i2c_transaction transport_trans;
     i2cmb_env_configuration configuration;
 
@@ -12,7 +12,7 @@ class i2cmb_predictor extends ncsu_component#(.T(wb_transaction));
         configuration = cfg;
     endfunction
 
-    virtual function void set_scoreboard(i2cmb_scoreboard scoreboard);
+    virtual function void set_scoreboard(ncsu_component #(i2c_transaction) scoreboard);
         this.scoreboard = scoreboard;
     endfunction
 
