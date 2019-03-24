0de76bbca240d24bd1d8d85f69c3db7e1f925557
vgshitol <vgshitol@ncsu.edu>
2019-03-23 22:52:54 -0400

    Structure Predictor

--- verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
+++ verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
@@ -3,6 +3,8 @@ class i2cmb_predictor extends ncsu_component#(.T(wb_transaction));
     ncsu_component#(.T(i2c_transaction)) scoreboard;
     i2c_transaction transport_trans;
     i2cmb_env_configuration configuration;
+    typedef enum {CHECK_START,PREDICT_DATA,STOP} stage_t;
+    stage_t state1 = CHECK_START;
 
     function new(string name = "", ncsu_component_base parent = null);
         super.new(name,parent);
@@ -18,7 +20,20 @@ class i2cmb_predictor extends ncsu_component#(.T(wb_transaction));
 
     virtual function void nb_put(T trans);
         $display({get_full_name()," ",trans.convert2string()});	
-	
+	case(state1)
+		CHECK_START: begin
+			
+		end
+		PREDICT_DATA : begin
+			
+		end
+		STOP : begin
+			
+		end
+		default: begin
+			
+		end
+	endcase
 //        scoreboard.nb_transport(trans, transport_trans);
     endfunction
 
