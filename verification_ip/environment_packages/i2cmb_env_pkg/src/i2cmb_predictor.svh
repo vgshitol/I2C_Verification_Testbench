e8acdd8145b955fbee18e8dee82876a1cc04e647
Vishal Shitole <vgshitol@ncsu.edu>
2019-03-24 19:53:28 -0400

    Predictor try

--- verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
+++ verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_predictor.svh
@@ -7,6 +7,9 @@ class i2cmb_predictor extends ncsu_component#(.T(wb_transaction));
     typedef enum {CHECK_START,GET_ADDRESS,GET_DATA,STOP} stage_t;
     stage_t state1 = CHECK_START;
 
+    static bit start_detected,stop_detected,address_calculated,repeated_start;
+    bit r_start_check;
+
     function new(string name = "", ncsu_component_base parent = null);
         super.new(name,parent);
         this.i2c_trans = new("name");
@@ -24,7 +27,7 @@ class i2cmb_predictor extends ncsu_component#(.T(wb_transaction));
     virtual function void nb_put(T trans);
 
         //$display({get_full_name()," ",trans.convert2string()});
-        case(state1)
+     /*   case(state1)
             GET_DATA : begin
                 //	$display("PREDICTOR : GET_DATA --> GET_DATA");
                 if(trans.address==2'b01) begin
@@ -68,6 +71,59 @@ class i2cmb_predictor extends ncsu_component#(.T(wb_transaction));
                 state1 = CHECK_START;
             end
         endcase
+        */
+
+        /******************************** Detect Start or repeated start******************************/
+        if(trans.addr==2'b10 && trans.data==8'b100)
+            begin
+                if(r_start_check==1)
+                    repeated_start=1;
+                else
+                    start_detected=1;
+                stop_detected=0;
+                address_calculated=0;
+                //$display("**********Predictor START*********");
+            end
+
+        /******************************** Detect Stop******************************/
+        if(trans.addr==2'b10 && trans.data==8'b101)
+            begin
+                stop_detected=1;
+                start_detected=0;
+                repeated_start=0;
+                address_calculated=0;
+                r_start_check=0;
+                //$display("**********Predictor STOP*********");
+            end
+        /******************************** Address Calculation ******************************/
+        if((start_detected ==1 || repeated_start==1) && trans.addr==2'b01)
+            begin
+                address_calculated=1;
+                if(trans.data[0]==0)
+                    i2c_trans.op=0;
+                else
+                    i2c_trans.op=1;
+                trans.data=trans.data>>1;
+                i2c_trans.i2c_address=trans.data[6:0];
+                repeated_start=0;
+                start_detected=0;
+            end
+            /******************************** Data Calculation ******************************/
+        else if(address_calculated==1 && stop_detected==0 && trans.addr==2'b01)
+            begin
+
+                i2c_trans.i2c_data={i2c_trans.i2c_data,trans.data};
+                r_start_check=1;
+
+            end
+
+        /***********************Sending the expected transaction to scoreboard ***************/
+
+        if(stop_detected==1 || repeated_start==1)
+            begin
+                scorebrd.nb_transport(i2c_trans, transport_trans);
+                i2c_trans=new("name");
+            end
 
     endfunction
 
